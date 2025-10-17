567f1e441bd3019c2021ea8240d248aca25ef060
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-10-17 17:21:51 +0200

    cleanup project superstructure

--- CMvPolynomial.lean
+++
@@ -1,8 +0,0 @@
--- This module serves as the root of the `CMvPolynomial` library.
--- Import modules here that should be built as part of the library.
-import CMvPolynomial.CMvMonomial
-import CMvPolynomial.CMvPolynomial
-import CMvPolynomial.Lawful
-import CMvPolynomial.MvPolyEquiv
-import CMvPolynomial.Unlawful
-import CMvPolynomial.Wheels
